08b30cde7e2d70c3388c809a0da0657bdec83cea
AnisDhia <50758880+AnisDhia@users.noreply.github.com>
2024-02-20 18:52:33 +0100

    Add basic movement + animations

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    private SpriteRenderer sr;
+    private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    private float dirX = 0f;
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+    private enum MovementState { idle, running, jumping, falling }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ? getaxisraw for no slide || getaxis for slide
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimationState();
+
+    }
+
+    private void UpdateAnimationState()
+    {
+        MovementState movementState;
+
+        if (dirX > 0f)
+        {
+            movementState = MovementState.running;
+            sr.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            movementState = MovementState.running;
+            sr.flipX = true;
+        }
+        else
+        {
+            movementState = MovementState.idle;
+        }
+
+        // ? Jumping animation has priority
+
+        if (rb.velocity.y > .1f)
+        {
+            movementState = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            movementState = MovementState.falling;
+        }
+
+        anim.SetInteger("MovementState", (int)movementState);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+}
